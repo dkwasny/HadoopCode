ce5f8b74822778345e9c583739237bc28767d98a
dkwasny <dkwasny67@gmail.com>
2016-01-29 16:32:39 -0500

    Only using runtime jars in job.sh

--- scripts/bash/job.sh
+++ scripts/bash/job.sh
@@ -20,7 +20,7 @@ shift;
 # Update the jar just in case...remove if compile time is unruly (unlikely)
 mvn -f $POM_FILE package;
 
-export HADOOP_CLASSPATH="$(mvn -f $POM_FILE dependency:build-classpath | grep -v INFO)";
+export HADOOP_CLASSPATH="$(mvn -f $POM_FILE dependency:build-classpath -DincludeScope=runtime | grep -v INFO)";
 LIB_JARS="$(echo $HADOOP_CLASSPATH | tr ':' ',')";
 
 hadoop jar $TARGET_DIR/*.jar $CLASS -libjars "$LIB_JARS" "$@";
